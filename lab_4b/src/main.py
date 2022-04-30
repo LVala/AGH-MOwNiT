32cb6c2a987e3f9772dc3bc25abc142de174c238
LVala <84684231+LVala@users.noreply.github.com>
2022-04-30 22:55:13 +0200

    Add part of spr_4b

--- lab_4b/src/main.py
+++ lab_4b/src/main.py
@@ -10,11 +10,11 @@ left_end = -6
 right_end = 6
 f = lambda x: x**2 - m*np.cos((np.pi*x)/k)
 
-for num_of_points in range(51,52):
+for num_of_points in range(51,60):
     eql_x, eql_y = gen_points_equally(f, left_end, right_end, num_of_points)
     weights = [1]*num_of_points
     degree = 20
     tri = trigpoly_approx(eql_x, eql_y, weights, degree)
 
-    plot_from_func([f, tri], ["f", "approximation with trigonometric polynomials of 3rd degree"], left_end, right_end, 2000, (eql_x, eql_y), f"{num_of_points} Points")
+    plot_from_func([f, tri], ["f", "approximation with trigonometric polynomials of 20th degree"], left_end, right_end, 2000, (eql_x, eql_y), f"{num_of_points} Points")
     # print(f"{num_of_points} points | SQR = {get_accuracy_sqr(f, tri, left_end, right_end, 1000):.3f}, ABS = {get_accuracy_abs(f, tri, left_end, right_end, 1000):.3f}")
