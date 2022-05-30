142fd553db5756be21cca4a85de56e5de6c14250
LVala <84684231+LVala@users.noreply.github.com>
2022-05-30 23:53:50 +0200

    Improvements to lab_7

--- lab_7/src/plotting.py
+++ lab_7/src/plotting.py
@@ -1,14 +1,11 @@
-from typing import Callable
-
-import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 
 def plot_from_array(array_x: list[float], array_y: list[list[float]], names: list[str]) -> None:
     plt.style.use('seaborn')
     fig, ax = plt.subplots()
-    plt.xlabel("y")
-    plt.ylabel("x")
+    plt.xlabel("błąd")
+    plt.ylabel("n")
 
     for arr, name, mcolor in zip(array_y, names, mcolors.BASE_COLORS.keys()):
         ax.plot(array_x, arr, color=mcolor, label=name,)
--- lab_7/src/utils.py
+++ lab_7/src/utils.py
@@ -1,7 +1,6 @@
 import numpy as np
 
-def jacobi_method(A: np.ndarray, B: np.ndarray, X: np.ndarray, mode: int, eps: float):
-    # matrix A must be diagonal dominant
+def jacobi_matrix(A: np.ndarray, B: np.ndarray, X: np.ndarray, mode: int, eps: float):
     D = np.diag(A)
     R = A - np.diagflat(D)
     iter_num = 0
@@ -16,30 +15,30 @@ def jacobi_method(A: np.ndarray, B: np.ndarray, X: np.ndarray, mode: int, eps: f
                 return X_new, iter_num
         X = X_new
 
-def get_spectral_radius(A: np.ndarray):
-    D = np.diag(A)
-    R = A - np.diagflat(D)
-    S = R / D
-    eigvals = np.linalg.eigvals(S)
-    return max(abs(i) for i in eigvals)
-
-def jacobi_2(A: np.ndarray, B: np.ndarray, X: np.ndarray, mode: int, eps: float):
+def jacobi_loop(A: np.ndarray, B: np.ndarray, X: np.ndarray, mode: int, eps: float):
     n = len(X)
     iter_num = 0
     while True:
-        X_old = np.copy(X)
+        X_new = np.zeros(n)
         for i in range(n):
             cos = 0
             for j in range(n):
                 if i != j:
                     cos += A[i,j]*X[j]
 
-            X[i] = (B[i]-cos)/A[i,i]
-
+            X_new[i] = (B[i]-cos)/A[i,i]
         iter_num += 1;
         if mode == 1:
-            if np.linalg.norm(X_old - X) < eps:
+            if np.linalg.norm(X_new - X) < eps:
                 return X, iter_num
         else:
-            if np.linalg.norm(A @ X - B) < eps:
+            if np.linalg.norm(A @ X_new - B) < eps:
                 return X, iter_num
+        X = X_new
+
+def get_spectral_radius(A: np.ndarray):
+    D = np.diag(A)
+    R = A - np.diagflat(D)
+    S = R / D
+    eigvals = np.linalg.eigvals(S)
+    return max(abs(i) for i in eigvals)
